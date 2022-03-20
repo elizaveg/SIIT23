49a3c186093c82bc018b38a2a288b57d18a74dcf
elizaveg <elizaveg@yahoo.com>
2022-03-20 19:33:09 +0200

    curs5

--- la curs/js/variabile.js
+++ la curs/js/variabile.js
@@ -5,3 +5,23 @@ let sirDeCaracter2 = 'Ana are mere';
 let boolTrue = true;
 let boolFalse = false;
 
+/*function par(nr) {
+    let rezultat;
+    if(nr % 2 === 0) {
+        rezultat = "par";
+    } else {
+        rezultat = "impar"
+    } 
+    return rezultate;
+}
+console.log(par(5)); */
+
+/*function fahrenheit(c) {
+    return ((c *9) / 5) + 32;
+}*/
+
+function greatest(nr) {
+    return max nr;
+}
+
+console.log(greatest(5, 8, 3));
