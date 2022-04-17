215ee71b9f97a484dae41b4847ed5e9074cde6ec
elizaveg <elizaveg@yahoo.com>
2022-04-17 18:04:09 +0300

    updates

---
+++ teme/phoneBook/phoneBook.js
@@ -0,0 +1,67 @@
+let state = {
+    list:[
+        {
+            nume:"Anastasia",
+            phone:"0712345678",
+        },
+        {
+            nume:"Cristian",
+            phone:"0712465748",
+        },
+    ],
+idxEdit:null,
+};
+
+function draw(){
+    let table = document.querySelector("#list tbody");
+    let str = "";
+    for(let i = 0; i < state.list.length; i++){
+        let elem = state.list[i];
+        str+=`
+            <tr>
+                <td>${elem.nume}</td>
+                <td>${elem.phone}</td>
+                <td>
+                    <button onclick="del(${i})">Delete</button>
+                    <button onclick="edit(${i})">Edit</button>
+                </td>
+            </tr>
+            `;
+    }
+    table.innerHTML = str;
+}
+
+function edit(idx) {
+    let elem = state.list[idx];
+    document.querySelector("[name='nume']").value = elem.nume;
+    document.querySelector("[name='phone']").value = elem.phone;
+    state.idxEdit = idx;
+}
+
+function del(idx){
+    if(
+        confirm(`Are you sure you want to delete this item: ${state.list[idx].nume}?`)) {
+        state.list.splice(idx,1);
+        draw();
+    }
+}
+
+function adauga(event) {
+    event.preventDefault();
+    let nume = document.querySelector("[name='nume']").value.trim();
+    let phone = document.querySelector("[name='phone']").value.trim();
+    if(state.idxEdit===null){
+        state.list.push({
+            nume: nume,
+            phone: phone,
+        });
+    } else {
+        state.list[state.idxEdit] = {
+            nume: nume,
+            phone: phone,
+        };
+        state.idxEdit = null;
+    }
+    document.querySelector("form").reset();
+    draw();
+}
