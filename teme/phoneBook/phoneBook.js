b8e637a31ed8645efb48fa57b6eec04665aade53
elizaveg <elizaveg@yahoo.com>
2022-04-21 13:52:12 +0300

    error class

--- teme/phoneBook/phoneBook.js
+++ teme/phoneBook/phoneBook.js
@@ -62,19 +62,53 @@ function adauga(event) {
     draw();
 }
 
-
 function showTable() {
     document.querySelector("#list").classList.remove("hidden");
 }
 
+function valideazaTelefon(input) {
+    let valid = true; // se porneste cu situatia ca este bine, pana cand..
+    //camp optional
+    if (input.value.length > 0) { //daca se scrie in camp se valideaza, daca n
+      for (let ch of input.value) {
+        if (ch < "0" || ch > "9") {
+          //fiecare caracter trebuie sa fie intre 0 si 9
+          valid = false; // caut un exemplu cand nu e ok
+        }
+      }
+      valid = valid && input.value.length === 10;
+    }
+    showError(input, valid);
+    //se da functia mea"input" si daca este valid sau nu
+  }
 
+  function valideazaNume(input) {
+    let valid = input.value.length >= 3;
+    showError(
+      input,
+      valid
+    );
+  }
 
+  function showError(input, valid) {
+    let span = input.nextElementSibling; //se ia din consola $0 (se selecteaza din elements elementul anterior si in consola se scrie $0)
+    if (valid) {
+      input.classList.remove("error"); //daca este valid se scoate error-ul
+      span.innerHTML = ""; //nu ia nici o valoare
+    } else {
+      input.classList.add("error"); //altfel adaug
+    }
+  }
 
-
-
-
-
-
+  function valideazaFormular(event) {
+    event.preventDefault(); //nu face submit la formular
+    //validare campuri
+    let nume = document.querySelector("[name='nume']");
+    let phone = document.querySelector("[name='phone']");
+    nume.onchange(); // valideazaNume(lastname);
+    phone.onchange(); //valideazaTelefon(phone);
+    }
+  
 
 
 
